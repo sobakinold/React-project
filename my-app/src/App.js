8f3555517659a13c9f07673d770f721c7b1f9fbd
Oleg <olegvolkov737@gmail.com>
2021-04-17 13:02:46 +0300

    Страница диалогов

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Profile from './components/Profile/Profile';
 import Aside from './components/Aside/Aside';
 import Header from './components/Header/Header';
+import Dialods from './components/Dialogs/Dialogs.jsx';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Aside />
-      <Profile />
+      <Dialods />
+      {/* <Profile /> */}
     </div >
   );
 }
---
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Dialogs.module.css';
+
+const Dialods = () => {
+    return (
+        <div className={classes.content}>
+            Dialods
+        </div>
+    )
+}
+
+export default Dialods;
