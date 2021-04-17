c8448a8761d549befd0de590dae6081155e6e9c7
Oleg <olegvolkov737@gmail.com>
2021-04-17 16:09:09 +0300

    test

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,41 @@ import classes from './Dialogs.module.css';
 const Dialods = () => {
     return (
         <div className={classes.content}>
-            Dialods
+            <div className={classes.header}>
+                Dialogs
+            </div>
+            <div className={classes.user_nav}>
+                <ul>
+                    <li>Дима</li>
+                    <li>Антон</li>
+                    <li>Светлана</li>
+                    <li>Сергей</li>
+                    <li>Владимир</li>
+                    <li>Маша</li>
+                    <li>Валера</li>
+                </ul>
+            </div>
+            <div className={classes.dialog_page}>
+                <div className={classes.avatar}>
+                    <img className={classes.img} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnSi-Gltn2w68Fn37i4rPk5IAW5xv9Xehwww&usqp=CAU' />
+                    <div className={classes.dialog}>
+                        Привет! Как дела?
+                    </div>
+                </div>
+                <div className={classes.avatar}>
+                    <img className={classes.img} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnSi-Gltn2w68Fn37i4rPk5IAW5xv9Xehwww&usqp=CAU' />
+                    <div className={classes.dialog}>
+                        Привет! Как дела?
+                    </div>
+                </div>
+                <div className={classes.avatar}>
+                    <img className={classes.img} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnSi-Gltn2w68Fn37i4rPk5IAW5xv9Xehwww&usqp=CAU' />
+                    <div className={classes.dialog}>
+                        Привет! Как дела?
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
