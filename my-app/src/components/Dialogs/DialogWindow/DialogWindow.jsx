cfd9f575743690864fdf79281cdc1b52e04285f4
Oleg <olegvolkov737@gmail.com>
2021-04-22 12:09:41 +0300

    Решение не выносить в модули содержание диологовой страницы

---
+++ my-app/src/components/Dialogs/DialogWindow/DialogWindow.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import classes from './DialogWindow.module.css';
+
+
+// const DialogWindow = (props) => {
+//     return (
+//         <div className={classes.dialog_window}>
+//             <div className={classes.dialog}>
+//                 {props.text}
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default DialogWindow;
--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,25 +1,45 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
-import DialogItems from './DialogItems/DialogItems';
-import DialogPage from './DialogPage/DialogPage';
+import { NavLink } from 'react-router-dom';
+// import DialogItems from './DialogItems/DialogItems';
+// import DialogWindow from './DialogWindow/DialogWindow';
 
+
+const DialogItems = (props) => {
+    let path = '/Dialogs/' + " " + props.id;
+    return (<div className={classes.dialog + " " + classes.active}><NavLink to={path}>{props.name}</NavLink></div>)
+}
+
+const DialogWindow = (props) => {
+    return (
+        <div className={classes.dialog_window}>
+            <div className={classes.dialog}>
+                {props.text}
+            </div>
+        </div>
+    )
+}
 
 const Dialogs = () => {
     return (
         <div className={classes.content}>
-            <div className={classes.header}>
-                Dialogs
+            < div className={classes.dialogs} >
+                <div className={classes.dialogItems}>
+                    {/* Создаю компонент "DialogItems" */}
+                    <DialogItems name='Дима' id='1' />
+                    <DialogItems name='Антон' id='2' />
+                    <DialogItems name='Светлана' id='3' />
+                    <DialogItems name='Сергей' id='4' />
+                    <DialogItems name='Владимир' id='5' />
+                    <DialogItems name='Маша' id='6' />
+                    <DialogItems name='Валерий' id='7' />
+                </div>
+            </ div>
+            <div className='DialogWindow'>
+                <DialogWindow text='Привет! Как дела?' />
+                <DialogWindow text='Зашибись! Сам как?' />
+                <DialogWindow text='Нормально' />
             </div>
-            {/* Создаю компонент "DialogItems" */}
-            <DialogItems name='Дима' id='1' />
-            <DialogItems name='Антон' id='2' />
-            <DialogItems name='Светлана' id='3' />
-            <DialogItems name='Сергей' id='4' />
-            <DialogItems name='Владимир' id='5' />
-            <DialogItems name='Маша' id='6' />
-            <DialogItems name='Валерий' id='7' />
-            {/* Создаю компонент "DialogPage" */}
-            <DialogPage />
         </div>
     )
 }
